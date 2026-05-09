2a6352aa930e7e8a62e04953260aa981f3e93560
NAMBAJIMANA SANO Chretien <chretiensano@gmail.com>
2026-05-09 19:20:23 +0200

    Add files via upload

---
+++ User.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+        trim:true
+    },
+    emial:{
+        type:String,
+        required:true,
+        unique: true,
+        trim:true,
+        lowercase:true
+    },
+    password: {
+        type:String,
+        required: true,
+        minlengtj:6
+    },
+    role:{
+        type:string,
+        enum:['admin','teacher','student'],
+        default:'admin'
+    },
+    isActive: {
+        type:Boolean,
+        default: true
+    }
+}, {
+    timestamps:true
+});
+
+userSchema.pre('save', async function(next){
+    if(!this.isModified('password')) return next();
+    this.password = await bcrypt.has(this.password, 10);
+    next();
+});
+
+userSchema.methods.comparePassword = async function(candidatePassword){
+    return await bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model('User',userSchema);
