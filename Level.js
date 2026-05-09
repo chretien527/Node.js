3d970c0631777aa662e5fb86746e04fd79a68cc2
NAMBAJIMANA SANO Chretien <chretiensano@gmail.com>
2026-05-09 19:19:32 +0200

    Add files via upload

---
+++ Level.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const levelSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        trim:true,
+        enum: ['Senior 4','Senior 5','Senior 6']
+    },
+    description :{
+        type:String,
+        trim:true
+    },
+    school:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'School',
+        required:true
+    }  
+}, {
+    timestamps:true
+});
+
+levelSchema.index({name: 1,shcool:1},{unique:true});
+
+module.exports = mongoose.model('Level',levelSchema);
